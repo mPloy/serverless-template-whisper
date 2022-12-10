21ec8ccfd0f62a2fdf0a7f17a98794d7eff9665a
mPloy <44012070+mPloy@users.noreply.github.com>
2022-12-09 17:06:15 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,28 +2,34 @@ import torch
 import whisper
 import os
 import base64
+import boto3
 from io import BytesIO
 
 # Init is ran on server startup
 # Load your model to GPU as a global variable here using the variable name "model"
 def init():
     global model
-    
+    global s3
+
     model = whisper.load_model("base")
+    s3 = boto3.client('s3')
 
 # Inference is ran for every server call
 # Reference your preloaded global model variable here.
 def inference(model_inputs:dict) -> dict:
     global model
+    global s3
 
     # Parse out your arguments
-    mp3BytesString = model_inputs.get('mp3BytesString', None)
-    if mp3BytesString == None:
+    s3Url = model_inputs.get('s3Url', None)
+    if s3Url == None:
         return {'message': "No input provided"}
-    
-    mp3Bytes = BytesIO(base64.b64decode(mp3BytesString.encode("ISO-8859-1")))
-    with open('input.mp3','wb') as file:
-        file.write(mp3Bytes.getbuffer())
+
+    # Extract the bucket and key from the S3 URL
+    bucket, key = s3Url.split('/')[2], '/'.join(s3Url.split('/')[3:])
+
+    # Download the file from S3
+    s3.download_file(bucket, key, 'input.mp3')
     
     # Run the model
     result = model.transcribe("input.mp3")
